f19a606eb135e94c3f162b6010939d45be0353d5
pavani1277 <85681249+pavani1277@users.noreply.github.com>
2021-06-20 13:47:57 +0530

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -1,6 +1,6 @@
 import java.util.*;
 class Sample {  
     public static void main(String args[]){  
-     System.out.println("Hello Java");  
+     System.out.println("Hello Java,This is Pavani");  
     }  
 }  
