5992de2ec8c23e79d2ca1f66b40c171fa2f784cb
harishbali1997@gmail.com <hemamounica1@gmail.com>
2021-07-18 16:08:56 +0530

    changing 2 files

--- Sample.java
+++ Sample.java
@@ -1,6 +1,6 @@
 import java.util.*;
 class Sample {  
     public static void main(String args[]){  
-     System.out.println("Hello Java,This is Pavani,changed by hari");  
+     System.out.println("Hello ,This is Pavani");  
     }  
 }  
